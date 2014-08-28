e5ed1c603a8c5f73bd76f02319a5ea1cd91a6dcb
Pamela Fox <pamela@khanacademy.org>
2014-08-28 10:12:21 -0700

    Fixes T3066 - Makes cursor usable
    Test Plan:
    - Write cursor(HAND); in program, see cursor change to hand

--- js/exec/babyhint.js
+++ js/exec/babyhint.js
@@ -85,6 +85,7 @@ var BabyHint = {
         "createFont": [1, 2],
         "cos": 1,
         "curve": 8,
+        "cursor": 1,
         "endShape": [0, 1],
         "dist": 4,
         "fill": [1, 3, 4],
