3271393ea70ae58bd175cc39ccce1bafd4080216
Alex Rodrigues <lemiant@gmail.com>
2014-10-29 10:51:27 -0700

    Re-ordered linting flow so that testing always occurs after injecting the code
    This change was made because the webpage tests should occur against the DOM,
    and the DOM must built built by injection, so that any scripts which assume document
    refers to their document run correctly
    WIP

--- build/js/live-editor.output.js
+++ build/js/live-editor.output.js
@@ -1,141 +1,5 @@
 this["Handlebars"] = this["Handlebars"] || {};
 this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
-this["Handlebars"]["templates"]["tipbar"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  var buffer = "", stack1, foundHelper, tmp1, self=this, functionType="function", blockHelperMissing=helpers.blockHelperMissing;
-
-function program1(depth0,data) {
-  
-  
-  return "Oh noes!";}
-
-function program3(depth0,data) {
-  
-  
-  return "Show me where";}
-
-  buffer += "<div class=\"tipbar\">\n    <div class=\"speech-arrow\"></div>\n    <div class=\"error-buddy\"></div>\n    <div class=\"tipnav\">\n        <a href=\"\" class=\"prev\"><span class=\"ui-icon ui-icon-circle-triangle-w\"></span></a>\n        <span class=\"current-pos\"></span>\n        <a href=\"\" class=\"next\"><span class=\"ui-icon ui-icon-circle-triangle-e\"></span></a>\n    </div>\n    <div class=\"text-wrap\">\n        <div class=\"oh-no\">";
-  foundHelper = helpers['_'];
-  stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(1, program1, data);
-  tmp1.hash = {};
-  tmp1.fn = tmp1;
-  tmp1.inverse = self.noop;
-  if(foundHelper && typeof stack1 === functionType) { stack1 = stack1.call(depth0, tmp1); }
-  else { stack1 = blockHelperMissing.call(depth0, stack1, tmp1); }
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "</div>\n        <div class=\"message\"></div>\n        <div class=\"show-me\"><a href>";
-  foundHelper = helpers['_'];
-  stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(3, program3, data);
-  tmp1.hash = {};
-  tmp1.fn = tmp1;
-  tmp1.inverse = self.noop;
-  if(foundHelper && typeof stack1 === functionType) { stack1 = stack1.call(depth0, tmp1); }
-  else { stack1 = blockHelperMissing.call(depth0, stack1, tmp1); }
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "</a></div>\n    </div>\n</div>";
-  return buffer;});;
-/**
- * This is called tipbar for historical reasons.
- * Originally, it appeared as a red bar sliding up from the bottom of the
- * canvas. Now it just powers the error reporting mechanism, which no longer
- * looks like a bar
- */
-
-window.TipBar = Backbone.View.extend({
-    initialize: function(options) {
-        this.output = options.output;
-        this.pos = 0;
-        this.texts = [];
-        this.render();
-        this.bind();
-    },
-
-    render: function() {
-        this.$overlay = $("<div class=\"overlay error-overlay\" style=\"display: none\"></div>").appendTo(this.$el);
-        this.$el.append(Handlebars.templates["tipbar"]());
-    },
-
-    bind: function() {
-        var self = this;
-
-        this.$el.on("click", ".tipbar .tipnav a", function() {
-            if (!$(this).hasClass("ui-state-disabled")) {
-                self.pos += $(this).hasClass("next") ? 1 : -1;
-                self.show();
-            }
-
-            self.output.postParent({ focus: true });
-
-            return false;
-        });
-
-        this.$el.on("click", ".tipbar .text-wrap a", function() {
-            var error = self.texts[self.pos];
-
-            self.output.postParent({ cursor: error });
-
-            return false;
-        });
-    },
-
-    show: function() {
-        var texts = this.errors;
-
-        var pos = this.pos;
-        var bar = this.$el.find(".tipbar");
-
-        // Inject current text
-        bar
-            .find(".current-pos").text(texts.length > 1 ? (pos + 1) + "/" + texts.length : "").end()
-            .find(".message").html(texts[pos].text || texts[pos] || "").end()
-            .find("a.prev").toggleClass("ui-state-disabled", pos === 0).end()
-            .find("a.next").toggleClass("ui-state-disabled", pos + 1 === texts.length).end();
-
-        this.$el.find(".show-me").toggle(texts[pos].row !== -1);
-
-        bar.find(".tipnav").toggle(texts.length > 1);
-
-        // Only animate the bar in if it's not visible
-        if (!bar.is(":visible")) {
-            bar
-                .css({ top: 400, opacity: 0.1 })
-                .show()
-                .animate({
-                    top: this.$el.find(".toolbar").is(":visible") ? 33 : 100,
-                    opacity: 0.9},
-                    300);
-        }
-    },
-
-    hide: function() {
-        var bar = this.$el.find(".tipbar");
-        if (bar.is(':visible')) {
-            bar.animate({ top: 400, opacity: 0.1 }, 300, function() {
-                $(this).hide();
-            });
-        }
-        clearTimeout(this.errorDelay);
-    },
-
-    toggleErrors: function(errors) {
-        this.errors = errors;
-        var hasErrors = !!errors.length;
-
-        this.$overlay.toggle(hasErrors);
-
-        if (!hasErrors) {
-            clearTimeout(this.errorDelay);
-            this.hide();
-            return;
-        }
-
-        this.errorDelay = setTimeout(this.show.bind(this), 1200);
-    }
-});
-this["Handlebars"] = this["Handlebars"] || {};
-this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
 this["Handlebars"]["templates"]["output"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers;
   var foundHelper, self=this;
@@ -639,49 +503,47 @@ window.LiveEditorOutput = Backbone.View.extend({
         
         this.currentCode = userCode;
 
-        var runDone = function(errors, testResults) {
-            errors = this.cleanErrors(errors || []);
+        var buildDone = function(errors) {
+            this.test(userCode, this.validate, errors, function(errors, testResults) {
+                errors = this.cleanErrors(errors || []);
 
-            if (!this.loaded) {
-                this.postParent({ loaded: true });
-                this.loaded = true;
-            }
-
-            // A callback for working with a test suite
-            if (callback) {
-                callback(errors, testResults);
-                return;
-            }
-
-            this.postParent({
-                results: {
-                    code: userCode,
-                    errors: errors,
-                    tests: testResults || [],
-                    assertions: this.assertions
+                if (!this.loaded) {
+                    this.postParent({ loaded: true });
+                    this.loaded = true;
                 }
-            });
 
-            this.toggle(!errors.length);
+                // A callback for working with a test suite
+                if (callback) {
+                    callback(errors, testResults);
+                    return;
+                }
+
+                this.postParent({
+                    results: {
+                        code: userCode,
+                        errors: errors,
+                        tests: testResults || [],
+                        assertions: this.assertions
+                    }
+                });
+
+                this.toggle(!errors.length);
+            }.bind(this));
         }.bind(this);
 
         this.lint(userCode, function(errors) {
-            // Run the tests (even if there are lint errors)
-            this.test(userCode, this.validate, errors, function(errors, testResults) {
-                if (errors.length > 0 || this.onlyRunTests) {
-                    return runDone(errors, testResults);
-                }
+            if (errors.length > 0 || this.onlyRunTests) {
+                return buildDone(errors);
+            }
 
-                // Then run the user's code
-                try {
-                    this.output.runCode(userCode, function(errors) {
-                        runDone(errors, testResults);
-                    });
-
-                } catch (e) {
-                    runDone([e], testResults);
-                }
-            }.bind(this));
+            // Then run the user's code
+            try {
+                this.output.runCode(userCode, function(errors) {
+                    buildDone(errors);
+                });
+            } catch (e) {
+                buildDone([e]);
+            }
         }.bind(this));
     },
 
