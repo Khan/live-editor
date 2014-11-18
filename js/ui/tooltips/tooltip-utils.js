01b64d04b755edefa3df3d5b662841bf6dc3871c
Alex Rodrigues <lemiant@gmail.com>
2014-11-17 22:50:57 -0800

    Fixed formattting of tooltip_utils
    Summary: Now that my linter is working fix style bugs.
    
    Test Plan:
    - Open webpage scratchpad
    - Open imageModal
    - See that scrollspy and lazy loading still works
    
    Reviewers: john, pamela
    
    Reviewed By: john, pamela
    
    Subscribers: john, #cs
    
    Maniphest Tasks: T3350
    
    Differential Revision: http://phabricator.khanacademy.org/D14654

--- js/ui/tooltips/tooltip-utils.js
+++ js/ui/tooltips/tooltip-utils.js
@@ -5,11 +5,11 @@ window.TooltipUtils = {
      * (although it is generally applicable to some degree)
      * It loads images as they are scrolled into view.
      * It makes the following assumptions
-     * - All lazy-loading images have a "data-lazy-src" with the src 
+     * - All lazy-loading images have a "data-lazy-src" with the src
      *     we wish to load on-demand
-     * - The div being scrolled is the offset parent of all the images 
+     * - The div being scrolled is the offset parent of all the images
      *     (http://api.jquery.com/position/)
-     * - If image A comes before image B in the source, 
+     * - If image A comes before image B in the source,
      *     then A is at least as high as B on the page.
      *
      */
@@ -30,15 +30,15 @@ window.TooltipUtils = {
                 } else {
                     return false; // break;
                 }
-            })
-        })
+            });
+        });
     },
 
     loadNow: function(img) {
         $.each($(img), function(i, elem) {
             $(elem).attr("src", $(elem).attr("data-lazy-src"));
             $(elem).removeAttr("data-lazy-src");
-        })
+        });
     },
 
     /**
@@ -49,7 +49,7 @@ window.TooltipUtils = {
      */
     setupScrollSpy: function(scrollables, arg) {
         $.each($(scrollables), function(i, shell) {
-            if (arg == "refresh") {
+            if (arg === "refresh") {
                 var navUl = $(shell).data("scrollspy.navUl");
                 if (!navUl) {
                     console.warn("tried to refresh scrollspy without first initializing it");
@@ -67,31 +67,31 @@ window.TooltipUtils = {
                 });
                 pointers.sort(function(a, b) {
                     return a[0] - b[0];
-                })
+                });
                 $(shell).data("scrollspy.pointers", pointers);
             } else {
                 var navUl = arg(shell);
                 $(shell).data("scrollspy.navUl", navUl);
-                $(shell).on("scroll", _.throttle(this.doScrollSpy, 60))
+                $(shell).on("scroll", _.throttle(this.doScrollSpy, 60));
                 $(navUl).find("li a").on("click", function(e) {
                     var top = $(shell).find($(this).attr("href")).position().top;
                     $(shell).scrollTop(top);
                     e.preventDefault();
-                })
+                });
             }
-        }.bind(this))
+        }.bind(this));
     },
 
     doScrollSpy: function() {
         var $this = $(this);
         var pointers = $this.data("scrollspy.pointers"); // [[height, node], ... ]
-        if (pointers == undefined) {
+        if (pointers === undefined) {
             $this.data("scrollspy.pointers", "working");
             setTimeout(function() {
-                TooltipUtils.setupScrollSpy($this, "refresh")
+                TooltipUtils.setupScrollSpy($this, "refresh");
             }, 0);
             return;
-        } else if (pointers == "working") {
+        } else if (pointers === "working") {
             return;
         }
 
@@ -108,4 +108,4 @@ window.TooltipUtils = {
         $this.data("scrollspy.navUl").find(".active").removeClass("active");
         $(active).closest("li").addClass("active");
     }
-}
+};
