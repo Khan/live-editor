a47f45c4fbea6196481031d765a62a79c7c881a7
John Resig <jeresig@gmail.com>
2015-07-13 14:51:27 -0700

    Adds in a new document mode and textarea editor.
    Summary:
    This adds in a new "document" mode to the editor. It's very simple, it has no output whatsoever (so there actually isn't a 'document' output module). Additionally I've added in a new "textarea" editor which is like a very simple version of Ace (but it supports non-monospaced fonts!). We're going to be using this for the new writing-based projects on KA.
    
    Most of the changes are simple, the biggest are:
    
    - We no longer require "execFile" and when it's not passed in we actually hide the output area (these are the changes to Handlebars and the CSS).
    - We have a new textarea editor mode. It handles most of the basic stuff properly (get/set text, get/set cursor).
    - Basic text screenshot support has been added to live-editor.js. This just takes the text from the user's program and turns it into a basic screenshot (See below).
    
    Example of screenshots in use:
    {F157558}
    
    Test Plan: The test suite runs the same as before! Honestly not sure what we'd test - this new mode is sort of the absence of any functionality. Suggestions recommended!
    
    Reviewers: kevinb, pamela, meredith
    
    Subscribers: #cs
    
    Differential Revision: https://phabricator.khanacademy.org/D19396

--- build/js/live-editor.ui.js
+++ build/js/live-editor.ui.js
@@ -150,19 +150,40 @@ this["Handlebars"]["templates"]["live-editor"] = Handlebars.template(function (H
 function program1(depth0,data) {
   
   
-  return "Loading...";}
+  return " no-output";}
 
 function program3(depth0,data) {
   
-  
-  return "Hmm...";}
+  var buffer = "", stack1;
+  buffer += "\n                <iframe id=\"output-frame\"\n                    src=\"";
+  foundHelper = helpers.execFile;
+  stack1 = foundHelper || depth0.execFile;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "execFile", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "\"\n                    data-src=\"";
+  foundHelper = helpers.execFile;
+  stack1 = foundHelper || depth0.execFile;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "execFile", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "\"></iframe>\n            ";
+  return buffer;}
 
 function program5(depth0,data) {
   
   
-  return "Restart";}
+  return "Loading...";}
 
 function program7(depth0,data) {
+  
+  
+  return "Hmm...";}
+
+function program9(depth0,data) {
+  
+  
+  return "Restart";}
+
+function program11(depth0,data) {
   
   var buffer = "", stack1;
   buffer += "\n                <a href=\"\" class=\"draw-color-button\" id=\"";
@@ -172,42 +193,52 @@ function program7(depth0,data) {
   buffer += escapeExpression(stack1) + "\">\n                    <span></span>\n                </a>\n                ";
   return buffer;}
 
-function program9(depth0,data) {
+function program13(depth0,data) {
   
   
   return "Record";}
 
-function program11(depth0,data) {
+function program15(depth0,data) {
   
   
   return "Loading...";}
 
-function program13(depth0,data) {
+function program17(depth0,data) {
   
   
   return "Enable Flash to load audio:";}
 
-function program15(depth0,data) {
+function program19(depth0,data) {
   
   
   return "Play";}
 
-function program17(depth0,data) {
+function program21(depth0,data) {
   
   
   return "Loading audio...";}
 
-  buffer += "<div class=\"scratchpad-wrap\">\n    <!-- Canvases (Drawing + Output) -->\n    <div class=\"scratchpad-canvas-wrap\">\n        <div id=\"output\">\n            <!-- Extra data-src attribute to work around\n                 cross-origin access policies. -->\n            <iframe id=\"output-frame\"\n                src=\"";
+  buffer += "<div class=\"scratchpad-wrap";
   foundHelper = helpers.execFile;
   stack1 = foundHelper || depth0.execFile;
-  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
-  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "execFile", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\"\n                data-src=\"";
+  stack2 = helpers.unless;
+  tmp1 = self.program(1, program1, data);
+  tmp1.hash = {};
+  tmp1.fn = tmp1;
+  tmp1.inverse = self.noop;
+  stack1 = stack2.call(depth0, stack1, tmp1);
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\">\n    <!-- Canvases (Drawing + Output) -->\n    <div class=\"scratchpad-canvas-wrap\">\n        <div id=\"output\">\n            <!-- Extra data-src attribute to work around\n                 cross-origin access policies. -->\n            ";
   foundHelper = helpers.execFile;
   stack1 = foundHelper || depth0.execFile;
-  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
-  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "execFile", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\"></iframe>\n            <canvas class=\"scratchpad-draw-canvas\" style=\"display:none;\"\n                width=\"400\" height=\"400\"></canvas>\n\n            <div class=\"overlay disable-overlay\" style=\"display:none;\">\n            </div>\n\n            <div class=\"scratchpad-canvas-loading\">\n                <img src=\"";
+  stack2 = helpers['if'];
+  tmp1 = self.program(3, program3, data);
+  tmp1.hash = {};
+  tmp1.fn = tmp1;
+  tmp1.inverse = self.noop;
+  stack1 = stack2.call(depth0, stack1, tmp1);
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n            <canvas class=\"scratchpad-draw-canvas\" style=\"display:none;\"\n                width=\"400\" height=\"400\"></canvas>\n\n            <div class=\"overlay disable-overlay\" style=\"display:none;\">\n            </div>\n\n            <div class=\"scratchpad-canvas-loading\">\n                <img src=\"";
   foundHelper = helpers.imagesDir;
   stack1 = foundHelper || depth0.imagesDir;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
@@ -215,7 +246,7 @@ function program17(depth0,data) {
   buffer += escapeExpression(stack1) + "/throbber-full.gif\">\n                <span class=\"hide-text\">";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(1, program1, data);
+  tmp1 = self.program(5, program5, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -226,7 +257,7 @@ function program17(depth0,data) {
   buffer += "\n                    ";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(3, program3, data);
+  tmp1 = self.program(7, program7, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -236,7 +267,7 @@ function program17(depth0,data) {
   buffer += "\n                </div>\n            </div>\n            <button id=\"restart-code\"\n                class=\"simple-button pull-right\">\n                <span class=\"icon-refresh\"></span>\n                ";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(5, program5, data);
+  tmp1 = self.program(9, program9, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -247,7 +278,7 @@ function program17(depth0,data) {
   foundHelper = helpers.colors;
   stack1 = foundHelper || depth0.colors;
   stack2 = helpers.each;
-  tmp1 = self.program(7, program7, data);
+  tmp1 = self.program(11, program11, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -256,7 +287,7 @@ function program17(depth0,data) {
   buffer += "\n            </div>\n\n            <!-- Record button -->\n            <button id=\"record\" class=\"simple-button pull-left\" style=\"display:none;\">";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(9, program9, data);
+  tmp1 = self.program(13, program13, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -271,7 +302,7 @@ function program17(depth0,data) {
   buffer += escapeExpression(stack1) + "/throbber-full.gif\">\n                <span class=\"hide-text\">";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(11, program11, data);
+  tmp1 = self.program(15, program15, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -281,7 +312,7 @@ function program17(depth0,data) {
   buffer += "</span>\n            </div>\n\n            <!-- This cannot be removed, if we want Flash to keep working! -->\n            <div id=\"sm2-container\">\n                ";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(13, program13, data);
+  tmp1 = self.program(17, program17, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -291,7 +322,7 @@ function program17(depth0,data) {
   buffer += "\n                <br>\n            </div>\n\n            <button class=\"scratchpad-editor-bigplay-button\" style=\"display:none;\">\n                <span class=\"icon-play\"></span>\n                <span class=\"hide-text\">";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(15, program15, data);
+  tmp1 = self.program(19, program19, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -301,7 +332,7 @@ function program17(depth0,data) {
   buffer += "</span>\n            </button>\n          </div>\n        </div>\n\n        <div class=\"scratchpad-toolbar\">\n            <!-- Row for playback controls -->\n            <div class=\"scratchpad-playbar\" style=\"display:none;\">\n                <div class=\"scratchpad-playbar-area\" style=\"display:none;\">\n                    <button\n                        class=\"simple-button primary scratchpad-playbar-play\"\n                        type=\"button\">\n                        <span class=\"icon-play\"></span>\n                    </button>\n\n                    <div class=\"scratchpad-playbar-progress\"></div>\n\n                    <span class=\"scratchpad-playbar-timeleft\"></span>\n                </div>\n                <div class=\"loading-msg\">\n                    ";
   foundHelper = helpers['_'];
   stack1 = foundHelper || depth0['_'];
-  tmp1 = self.program(17, program17, data);
+  tmp1 = self.program(21, program21, data);
   tmp1.hash = {};
   tmp1.fn = tmp1;
   tmp1.inverse = self.noop;
@@ -861,7 +892,7 @@ window.LiveEditor = Backbone.View.extend({
         this.externalsDir = this._qualifyURL(options.externalsDir);
         this.imagesDir = this._qualifyURL(options.imagesDir);
         this.soundsDir = options.soundsDir;
-        this.execFile = this._qualifyURL(options.execFile);
+        this.execFile = options.execFile ? this._qualifyURL(options.execFile) : "";
         this.jshintFile = this._qualifyURL(options.jshintFile || this.externalsDir + "jshint/jshint.js");
         this.redirectUrl = options.redirectUrl;
 
@@ -2061,6 +2092,10 @@ window.LiveEditor = Backbone.View.extend({
         this.$el.find("#output-frame")[0].contentWindow.postMessage(JSON.stringify(data), this.postFrameOrigin());
     },
 
+    hasFrame: function hasFrame() {
+        return !!this.execFile;
+    },
+
     /*
      * Restart the code in the output frame.
      */
@@ -2172,6 +2207,45 @@ window.LiveEditor = Backbone.View.extend({
     },
 
     getScreenshot: function getScreenshot(callback) {
+        // If we don't have an output frame then we need to render our
+        // own screenshot (so we just use the text in the editor)
+        if (!this.hasFrame()) {
+            var canvas = document.createElement("canvas");
+            canvas.width = 200;
+            canvas.height = 200;
+            var ctx = canvas.getContext("2d");
+
+            // Default sizing, we also use a 5px margin
+            var lineHeight = 24;
+            var maxWidth = 190;
+
+            // We go through all of this so that the text will wrap nicely
+            var text = this.editor.text();
+            var words = text.split(/\s+/);
+            var lines = 0;
+            var currentLine = words[0];
+
+            ctx.font = lineHeight + "px serif";
+
+            for (var i = 1; i < words.length; i++) {
+                var word = words[i];
+                var width = ctx.measureText(currentLine + " " + word).width;
+                if (width < maxWidth) {
+                    currentLine += " " + word;
+                } else {
+                    lines += 1;
+                    ctx.fillText(currentLine, 5, (lineHeight + 5) * lines);
+                    currentLine = word;
+                }
+            }
+            lines += 1;
+            ctx.fillText(currentLine, 5, (lineHeight + 5) * lines + 5);
+
+            // Render it to an image and we're done!
+            callback(canvas.toDataURL("image/png"));
+            return;
+        }
+
         // Unbind any handlers this function may have set for previous
         // screenshots
         $(window).off("message.getScreenshot");
