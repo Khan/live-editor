20da1b115e97cd62009b0dd995c22cb6486c11e0
Kevin Barabash <kevinb@khanacademy.org>
2015-07-13 12:17:47 -0600

    Check that a sound is in our list of sounds before trying to load it.
    Summary:
    phantomjs was timing out when trying to load sounds that don't exist.  It
    also doesn't make sense to try and load sounds that we know don't exist.
    Instead, we check if a sound is in OutputSounds before trying to load it.
    We should probably do the same for images, but I've left that for a future
    diff.
    
    Test Plan: - gulp test_output_pjs, verify that there are no failures
    
    Reviewers: pamela
    
    Reviewed By: pamela
    
    Subscribers: john
    
    Differential Revision: https://phabricator.khanacademy.org/D19365

--- build/js/live-editor.shared.js
+++ build/js/live-editor.shared.js
@@ -50,6 +50,29 @@ window.ExtendedOutputImages = [{
         images: "fireworks-2015 fireworks-in-sky fireworks-over-harbor fireworks-scattered gingerbread-family gingerbread-house gingerbread-houses gingerbread-man hannukah-dreidel hannukah-menorah house-with-lights reindeer snow-crystal1 snow-crystal2 snow-crystal3 snowy-slope-with-trees stocking-empty xmas-cookies xmas-ornament-boat xmas-ornament-on-tree xmas-ornaments xmas-presents xmas-scene-holly-border xmas-tree-with-presents xmas-tree xmas-wreath".split(" ")
     }]
 }];
+if (!$._) {
+    $._ = function (msg) {
+        return msg;
+    };
+}
+
+// The master list of acceptable sounds
+// Build a list of all the available sounds
+// TODO(kevinb) add methods to help query for sounds
+window.OutputSounds = [{
+    className: "Sound effects",
+    groups: [{
+        groupName: "rpg",
+        sounds: "battle-magic battle-spell battle-swing coin-jingle door-open giant-hyah giant-no giant-yah hit-clop hit-splat hit-thud hit-whack metal-chime metal-clink step-heavy water-bubble water-slosh".split(" "),
+        cite: $._("'RPG Sound Effects' sounds by artisticdude"),
+        citeLink: "http://opengameart.org/content/rpg-sound-pack"
+    }, {
+        groupName: "retro",
+        sounds: "boom1 boom2 coin hit1 hit2 jump1 jump2 laser1 laser2 laser3 laser4 rumble thruster-short thruster-long whistle1 whistle2".split(" "),
+        cite: $._("'Retro Game Sounds' sounds by spongejr"),
+        citeLink: "https://www.khanacademy.org/profile/spongejr/"
+    }]
+}];
 // Note: All time measurements are handled in milliseconds
 window.ScratchpadRecord = Backbone.Model.extend({
     initialize: function initialize() {
