629903b6730323ee99b74048c6dc0b2810b1c20e
Jason Won <jason.j.won@gmail.com>
2015-08-23 14:24:26 -0700

    fixed in-page links, and a typo

--- external/slowparse/slowparse.js
+++ external/slowparse/slowparse.js
@@ -1751,11 +1751,17 @@
         }
 
         //Add a new validator to check if there is invalid link content
-        if (nameTok.value === "href" || nameTok.value === "src") {
-          if (!valueTok.match(regexp)) {
-            //Cheers to Diego Perini: https://gist.github.com/dperini/729294
-            var regexp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+        //Cheers to Diego Perini: https://gist.github.com/dperini/729294
+        var regexp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+        if (!valueTok.value.match(regexp)) {
+          if (nameTok.value === "src"){
             throw new ParseError("INVALID_URL", this, nameTok, valueTok);
+          } else if (nameTok.value === "href") {
+            var url = valueTok.value.substring(1, valueTok.value.length-1);
+            if (url.charAt(0) !== "#" && url.substring(0, 10) !== "javascript") {
+              throw new ParseError("INVALID_URL", this, nameTok, valueTok);
+            }
           }
         }
 
