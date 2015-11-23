2c013923595f743f17da89dcce67dfc2a8aba762
Kevin Barabash <kevinb@khanacademy.org>
2015-11-22 20:36:57 -0800

    Provide	a way for external contributors	to test	changes	against	existing scratchpads
    Summary:
    demos/simple/index.html	now recognizes a 'scratchpad' URL param	which accepts a
    scratchpad id as the parameter.  I've also added a scratchpads.html which provides
    links to all of	 the scratches currently   labeled as 'challenge', 'official', or 'tutorial'.
    
    Test Plan:
    - load demos/simple/scratchpads.html
    - verify that all three columns have multiple linkts to scratchpads
    - click on one of the links in each column and verify that it loads the program and that it runs
    
    Auditors: pamela

---
+++ demos/simple/scratchpads.js
@@ -0,0 +1,34 @@
+var challengesDiv = document.getElementById('challenges');
+var officialDiv = document.getElementById('official');
+var tutorialsDiv = document.getElementById('tutorials');
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "scratchpads.json", false);
+xhr.send();
+
+var scratchads = JSON.parse(xhr.responseText);
+var urlBase = "index.html?scratchpad=";
+
+scratchads.challenges.forEach(function(scratchpad) {
+    var a = document.createElement('a');
+    a.href = urlBase + scratchpad.id;
+    a.innerText = scratchpad.title;
+    challengesDiv.appendChild(a);
+    challengesDiv.appendChild(document.createElement('br'));
+});
+
+scratchads.official.forEach(function(scratchpad) {
+    var a = document.createElement('a');
+    a.href = urlBase + scratchpad.id;
+    a.innerText = scratchpad.title;
+    officialDiv.appendChild(a);
+    officialDiv.appendChild(document.createElement('br'));
+});
+
+scratchads.tutorials.forEach(function(scratchpad) {
+    var a = document.createElement('a');
+    a.href = urlBase + scratchpad.id;
+    a.innerText = scratchpad.title;
+    tutorialsDiv.appendChild(a);
+    tutorialsDiv.appendChild(document.createElement('br'));
+});
