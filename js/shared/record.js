209ac1033715120cde2c5e5bb9192fd27494d65c
Scott Grant <scottgrant@khanacademy.org>
2018-07-20 12:04:05 -0400

    Uses window.requestAnimationFrame to prevent frozen script errors while seeking
    We've had some reports about the screen locking up while seeking ahead. This is caused by processing commands and building the cache, which trap us in the single thread and prevent other behaviour.
    
    This approach uses `window.requestAnimationFrame` to process a few commands and cache entries, then see if we need to update the browser before trying another pass.
    
    I played with the value of `animationStep` and this seemed okay! It might make sense to show more or less though, so that would be a value to play around with. There's also some place where it's possible to generate bad cache entries if you continue to seek while it's still processing stuff. You can see this if you visit a Khan page like http://localhost:8081/computing/computer-programming/programming/drawing-basics/p/making-drawings-with-code, seek ahead to the end, and while it's processing, start scrolling around the scrollbar. That might be a problem in our Khan `webapp` though; I wasn't sure if I missed returning out of somewhere here in `live-editor` while seeking like in the `play` function, or if this is coming from the scroll bar in `webapp`.

--- js/shared/record.js
+++ js/shared/record.js
@@ -21,6 +21,10 @@ window.ScratchpadRecord = Backbone.Model.extend({
         // Array of command arrays for each chunk. Only holds chunks that
         // have been saved.
         this.allSavedCommands = [];
+
+        // True when we actively seeking to a new position and potentially
+        // building the cache.
+        this.seeking = false;
     },
 
     setActualInitData: function(actualData) {
@@ -141,6 +145,12 @@ window.ScratchpadRecord = Backbone.Model.extend({
     // Seek to a given position in the playback, executing all the
     // commands in the interim
     seekTo: function(time) {
+        if (this.seeking) {
+            return;
+        }
+
+        this.seeking = true;
+
         // Initialize and seek to the desired position
         this.pauseTime = (new Date()).getTime();
         this.playStart = this.pauseTime - time;
@@ -188,13 +198,27 @@ window.ScratchpadRecord = Backbone.Model.extend({
             this.cacheRestore(-1 * this.seekCacheInterval);
         }
 
-        // Execute commands and build cache, bringing state up to current
-        for (var i = cacheOffset; i <= seekPos; i++) {
-            this.runCommand(this.commands[i]);
-            this.cache(i);
+        let currentOffset = cacheOffset;
+        const buildCache = () => {
+            const animationStep = 100;
+            const steps = Math.min(animationStep, seekPos - currentOffset + 1);
+            const newOffset = currentOffset + steps;
+
+            // console.log(`${currentOffset} / ${newOffset} / ${seekPos}`);
+            for (; currentOffset < newOffset; currentOffset += 1) {
+                this.runCommand(this.commands[currentOffset]);
+                this.cache(currentOffset);
+            }
+
+            if (currentOffset <= seekPos) {
+                window.requestAnimationFrame(buildCache);
+            } else {
+                this.seeking = false;
+                this.trigger("seekDone");
+            }
         }
 
-        this.trigger("seekDone");
+        window.requestAnimationFrame(buildCache);
     },
 
     // Cache the result of the specified command
@@ -232,7 +256,7 @@ window.ScratchpadRecord = Backbone.Model.extend({
 
     play: function() {
         // Don't play if we're already playing or recording
-        if (this.recording || this.playing || !this.commands ||
+        if (this.recording || this.playing || this.seeking || !this.commands ||
                 this.commands.length === 0) {
             return;
         }
