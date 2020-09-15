2e161fac68c13530b205e12fdeae71aa0675d80d
Pamela Fox <pamela.fox@gmail.com>
2020-09-15 09:44:56 -0700

    Upgrade gulp to gulp 4

--- testutil/gulp-mocha-runner.js
+++ testutil/gulp-mocha-runner.js
@@ -78,7 +78,26 @@ var plugin = function (options) {
             }
 
             const chrome = await launchChrome();
-            const protocol = await CDP({port: chrome.port});
+
+            let protocol = null;
+            let connectAttempts = 0;
+            while (!protocol) {
+              // Workaround for intermittent CI error caused by
+              // https://github.com/GoogleChrome/chrome-launcher/issues/145
+              try {
+                protocol = await CDP({ port: chrome.port });
+              } catch (e) {
+                if (e.message === "No inspectable targets" && connectAttempts < 50) {
+                  connectAttempts++;
+                  console.log(
+                    "Unable to establish connection to chrome target - trying again..."
+                  );
+                  await new Promise(resolve => setTimeout(resolve, 100));
+                } else {
+                  throw e;
+                }
+              }
+            }
             const {
                 DOM,
                 Page,
